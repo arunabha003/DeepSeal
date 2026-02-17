d1d32a20770387887af23189ab3f50a45c7ee6b7
Arunabha <arunabhadhal04@gmail.com>
2026-02-17 20:57:52 +0530

    chore: stabilize erc8004 agent registration in deploy script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -17,6 +17,9 @@ contract Deploy is Script {
     struct DeployConfig {
         uint256 deployerKey;
         address deployer;
+        uint256 agentRegistrarKey;
+        address agentRegistrar;
+        uint256 agentRegistrarFundingWei;
         address reportForwarder;
         bytes32 expectedWorkflowId;
         address expectedAuthor;
@@ -55,6 +58,10 @@ contract Deploy is Script {
     function _readConfig() internal returns (DeployConfig memory cfg) {
         cfg.deployerKey = vm.envUint("PRIVATE_KEY");
         cfg.deployer = vm.addr(cfg.deployerKey);
+        cfg.agentRegistrarKey =
+            vm.envOr("AGENT_REGISTRAR_PRIVATE_KEY", uint256(keccak256("RWA_AGENT_REGISTRAR_PRIVATE_KEY_V1")));
+        cfg.agentRegistrar = vm.addr(cfg.agentRegistrarKey);
+        cfg.agentRegistrarFundingWei = vm.envOr("AGENT_REGISTRAR_FUNDING_WEI", uint256(0.02 ether));
         cfg.reportForwarder = vm.envOr("CRE_REPORT_FORWARDER", address(0));
         cfg.expectedWorkflowId = vm.envOr("CRE_WORKFLOW_ID", bytes32(0));
         cfg.expectedAuthor = vm.envOr("CRE_WORKFLOW_AUTHOR", address(0));
@@ -93,18 +100,35 @@ contract Deploy is Script {
         out.validationRegistry = new ValidationRegistry();
         out.validationRegistry.initialize(address(out.identityRegistry));
 
-        out.reputationAgentId = out.identityRegistry.register(cfg.reputationAgentUri);
-        out.validationAgentId = out.identityRegistry.register(cfg.validationAgentUri);
-
-        out.identityRegistry.approve(address(out.receiver), out.validationAgentId);
+        _registerERC8004Agents(cfg, out);
         out.receiver.setERC8004Reputation(address(out.reputationRegistry), out.reputationAgentId, 0);
         out.receiver.setERC8004Validation(address(out.validationRegistry), out.validationAgentId, address(out.receiver), true);
 
         out.asset.mint(cfg.deployer, cfg.seedAmount);
     }
 
+    function _registerERC8004Agents(DeployConfig memory cfg, DeployArtifacts memory out) internal {
+        require(cfg.agentRegistrar != address(0), "AGENT_REGISTRAR_ZERO");
+        require(cfg.agentRegistrar.code.length == 0, "AGENT_REGISTRAR_MUST_BE_EOA");
+
+        uint256 registrarBalance = cfg.agentRegistrar.balance;
+        if (registrarBalance < cfg.agentRegistrarFundingWei) {
+            (bool funded,) = payable(cfg.agentRegistrar).call{value: cfg.agentRegistrarFundingWei - registrarBalance}("");
+            require(funded, "AGENT_REGISTRAR_FUNDING_FAILED");
+        }
+
+        vm.stopBroadcast();
+        vm.startBroadcast(cfg.agentRegistrarKey);
+        out.reputationAgentId = out.identityRegistry.register(cfg.reputationAgentUri);
+        out.validationAgentId = out.identityRegistry.register(cfg.validationAgentUri);
+        out.identityRegistry.approve(address(out.receiver), out.validationAgentId);
+        vm.stopBroadcast();
+        vm.startBroadcast(cfg.deployerKey);
+    }
+
     function _print(DeployConfig memory cfg, DeployArtifacts memory out) internal {
         console2.log("Deployer:", cfg.deployer);
+        console2.log("ERC8004 AgentRegistrar:", cfg.agentRegistrar);
         console2.log("CREReportForwarder:", cfg.reportForwarder);
         console2.logBytes32(cfg.expectedWorkflowId);
         console2.log("ExpectedAuthor:", cfg.expectedAuthor);
